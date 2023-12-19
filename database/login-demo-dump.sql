63a4ab936712367a016f5889c156d4a40fe0bd70
Rose Pezzuti Dyer <pezzutidyer@uchicago.edu>
2023-12-18 18:22:54 -0600

    db schema

---
+++ database/login-demo-dump.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE  IF NOT EXISTS `logindemo` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `logindemo`;
+-- Host: localhost    Database: logindemo
+-- ------------------------------------------------------
+-- Server version	5.7.39
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tAccounts`
+--
+
+DROP TABLE IF EXISTS `tAccounts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tAccounts` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `CreateDate` datetime DEFAULT CURRENT_TIMESTAMP,
+  `Email` varchar(300) DEFAULT NULL,
+  `HashedPassword` varchar(300) DEFAULT NULL,
+  `ConfirmationKey` varchar(300) DEFAULT NULL,
+  `ConfirmationDate` datetime DEFAULT NULL,
+  `IsActive` tinyint(4) DEFAULT NULL,
+  `PasswordResetRequestDate` datetime DEFAULT NULL,
+  `PasswordResetKey` varchar(300) DEFAULT NULL,
+  `PasswordResetDate` datetime DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-12-18 18:21:00
